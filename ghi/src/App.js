a306ed55eb86a387d2cda6bc0c931ffc8d5caed7
Wenqi Jin <kristenjinn@gmail.com>
2023-02-08 16:22:57 -0800

    deploying

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -12,7 +12,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
-      <BrowserRouter>
+      <BrowserRouter basename="/snowseekers/">
         <Nav/>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"/>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
