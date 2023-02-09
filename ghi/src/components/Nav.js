7e4b469f1640b128bf5fa19ce8ddb685ca5dcfc8
Wenqi Jin <kristenjinn@gmail.com>
2023-02-08 16:51:18 -0800

    deploying

--- ghi/src/components/Nav.js
+++ ghi/src/components/Nav.js
@@ -1,14 +1,14 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import Logo from '../myLogo.svg'
 
 
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/">
+      <Link className="navbar-brand" to="/">
         <img src={Logo} width="30" height="30" className="d-inline-block align-top" alt="Logo"/>
-        SnowSeeker
-      </a>
+        SnowSeekers
+      </Link>
       <div className="btn-group">
         <NavLink to="all/" className="nav-link" style={({ isActive }) => ({color: isActive ? 'black' : 'grey' })}>
           Ski resorts
